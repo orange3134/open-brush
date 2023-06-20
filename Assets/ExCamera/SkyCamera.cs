b2c662619d623f9fb645f4f149c66a4495abad2d
orange <mikan3134@gmail.com>
2023-06-20 16:17:50 +0900

    SkyCameraの初期実装

---
+++ Assets/ExCamera/SkyCamera.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkyCamera
+{
+    public class SkyCamera : MonoBehaviour
+    {
+        private Transform _cameraParent;
+        private Transform _pitchRoll;
+        private Camera _mainCamera;
+
+        private Vector3 _prevPositionVector;
+        private Vector3 _currentMoveVelocity;
+        private Vector3 _prevRotationVector;
+        private Vector3 _currentRotationVelocity;
+
+        [SerializeField] private bool _horizontalMove = true;
+        [SerializeField] private float _moveSpeed = 0.2f;
+        [SerializeField] private float _moveSmoothTime = 0.1f;
+        [SerializeField] private float _rotationSpeed = 0.2f;
+        [SerializeField] private float _rotationSmoothTime = 0.1f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _cameraParent = this.transform.Find("CameraParent");
+            _pitchRoll = _cameraParent.Find("PitchRoll");
+            _mainCamera = _pitchRoll.Find("Camera").GetComponent<Camera>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void Move(Vector3 positionVector)
+        {
+            Transform moveSpace = _horizontalMove ? _cameraParent : _pitchRoll;
+
+            Vector3 positionVectorInSpace = _cameraParent.InverseTransformVector(moveSpace.TransformVector(positionVector));
+
+            Vector3 smoothPositionVector = Vector3.SmoothDamp(_prevPositionVector, positionVectorInSpace * _moveSpeed, ref _currentMoveVelocity, _moveSmoothTime);
+
+            Debug.Log("PositionVector: " + positionVector + " smoothPositionVector: " + smoothPositionVector);
+            _cameraParent.Translate(smoothPositionVector);
+            _prevPositionVector = smoothPositionVector;
+        }
+
+        //x=yaw y=pitch z=roll
+        public void Rotation(Vector3 rotationVector)
+        {
+            Vector3 smoothRotationVector = Vector3.SmoothDamp(_prevRotationVector, rotationVector * _rotationSpeed, ref _currentRotationVelocity, _rotationSmoothTime);
+            _prevRotationVector = smoothRotationVector;
+
+            Quaternion yawRotation = Quaternion.Euler(0, smoothRotationVector.x, 0);
+            Quaternion pitchRollRotation = Quaternion.Euler(smoothRotationVector.y, 0, smoothRotationVector.z);
+
+            ParentYawRotation(yawRotation);
+            PitchRollRotation(pitchRollRotation);
+
+        }
+
+        private void ParentYawRotation(Quaternion rotation)
+        {
+            _cameraParent.SetLocalPositionAndRotation(_cameraParent.localPosition, rotation * _cameraParent.localRotation);
+        }
+
+        private void PitchRollRotation(Quaternion rotation)
+        {
+            _pitchRoll.SetLocalPositionAndRotation(_pitchRoll.localPosition, _pitchRoll.localRotation * rotation);
+        }
+
+        public void ResetRollRotation()
+        {
+            var currentEuler = _pitchRoll.localRotation.eulerAngles;
+            _pitchRoll.SetLocalPositionAndRotation(_pitchRoll.localPosition, Quaternion.Euler(currentEuler.x, 0f, 0f));
+        }
+    }
+}
+
---
+++ Assets/ExCamera/SkyCameraControllerDesktop.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SkyCamera
+{
+    public class SkyCameraControllerDesktop : MonoBehaviour
+    {
+        private SkyCamera _skyCamera;
+        
+        [SerializeField] private bool _invertYAxis = true;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _skyCamera = GetComponent<SkyCamera>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(_skyCamera != null)
+            {
+                Vector3 inputVector = MoveInput();
+                Vector3 rotationInput = RotationInput();
+                _skyCamera.Move(inputVector);
+                _skyCamera.Rotation(rotationInput);
+
+                if (Keyboard.current.zKey.wasPressedThisFrame) _skyCamera.ResetRollRotation();
+            }
+
+        }
+
+        private Vector3 MoveInput()
+        {
+            float forward = (Keyboard.current.wKey.isPressed ? 1f : 0f) + (Keyboard.current.sKey.isPressed ? -1f : 0f);
+            float right = (Keyboard.current.dKey.isPressed ? 1f : 0f) + (Keyboard.current.aKey.isPressed ? -1f : 0f);
+            float up = (Keyboard.current.spaceKey.isPressed ? 1f : 0f) + (Keyboard.current.cKey.isPressed ? -1f : 0f);
+            Vector3 vector = new Vector3(right,up,forward);
+            return vector;
+        }
+
+        private Vector3 RotationInput()
+        {
+            float yaw = (Keyboard.current.rightArrowKey.isPressed ? 1f : 0f) + (Keyboard.current.leftArrowKey.isPressed ? -1f : 0f);
+            float pitch = (Keyboard.current.downArrowKey.isPressed ? 1f : 0f) + (Keyboard.current.upArrowKey.isPressed ? -1f : 0f);
+            float roll = (Keyboard.current.qKey.isPressed ? 1f : 0f) + (Keyboard.current.eKey.isPressed ? -1f : 0f);
+            Vector3 vector = new Vector3(yaw, _invertYAxis ? -pitch : pitch, roll);
+            return vector;
+        }
+    }
+}
+
